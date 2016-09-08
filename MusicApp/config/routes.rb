9b9d606f5607c4ccab12864df262bab9ff5c1a2d
Yisheng Cai <cai.eason@gmail.com>
2016-09-08 11:22:34 -0400

    Add new routes

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -43,6 +43,16 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new, :show]
   resource :session, only: [:create, :new, :destroy]
 
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+  resources :albums, only: [:create, :edit, :show, :update, :destroy] do
+    resources :tracks, only: [:new]
+  end
+  resources :tracks, only: [:create, :edit, :show, :update, :destroy]
+
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
